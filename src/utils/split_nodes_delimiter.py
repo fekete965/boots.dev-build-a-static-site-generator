d71e5615cde80c2bfa8a696e327061c045478835
Bence Fekete <fekete965@gmail.com>
2025-10-02 11:51:06 +0100

    Improve split_nodes_delimiter

--- src/utils/split_nodes_delimiter.py
+++ src/utils/split_nodes_delimiter.py
@@ -1,4 +1,4 @@
-from nodes.textnode import TextNode, TextType
+from src.nodes.textnode import TextNode, TextType
 
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
@@ -12,15 +12,30 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
         chunks = old.text.split(delimiter)
         chunks_len = len(chunks)
 
+        if chunks_len == 0:
+            continue
+
+        if chunks_len == 1 and chunks[0] == "":
+            continue
+
+        if chunks_len == 1 and chunks[0] != "":
+            final_nodes.append(TextNode(text=chunks[0], type=TextType.TEXT))
+            continue
+
         if chunks_len % 2 == 0:
             raise Exception("Invalid Markdown syntax")
 
         for i in range(0, chunks_len):
+            text = chunks[i]
+
+            if text == "":
+                continue
+
             if i % 2 == 0:
                 final_nodes.append(
-                    TextNode(text=chunks[i], type=TextType.TEXT)
+                    TextNode(text=text, type=TextType.TEXT)
                 )
             else:
-                final_nodes.append(TextNode(text=chunks[i], type=text_type))
+                final_nodes.append(TextNode(text=text, type=text_type))
 
     return final_nodes
--- src/utils/test_split_nodes_delimiter.py
+++ src/utils/test_split_nodes_delimiter.py
@@ -1,10 +1,36 @@
 import unittest
 
-from nodes.textnode import TextNode, TextType
+from src.nodes.textnode import TextNode, TextType
 from .split_nodes_delimiter import split_nodes_delimiter
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter_with_invalid_markdown_syntax(self):
+        node = TextNode(
+            text="_I am utterly alone",
+            type=TextType.TEXT,
+        )
+
+        try:
+            split_nodes_delimiter([node], "_", TextType.ITALIC)
+        except Exception as e:
+            self.assertEqual(str(e), "Invalid Markdown syntax")
+
+    def test_split_nodes_delimiter_with_single_chunk(self):
+        node = TextNode(
+            text="_I am utterly alone_",
+            type=TextType.TEXT,
+        )
+        new_nodes = split_nodes_delimiter(
+            old_nodes=[node],
+            delimiter="_",
+            text_type=TextType.ITALIC,
+        )
+        self.assertEqual(len(new_nodes), 1)
+
+        self.assertEqual(new_nodes[0].type, TextType.ITALIC)
+        self.assertEqual(new_nodes[0].text, "I am utterly alone")
+
     def test_split_nodes_delimiter_italic(self):
         node = TextNode(
             text="This is simple text with a _italic_ word",
