e236fad48783abbdf4eb39c654330414ece078c8
Bence Fekete <fekete965@gmail.com>
2025-11-10 09:49:11 +0000

    Fix bold file splitting in text_to_text_nodes

--- src/utils/text_to_textnodes.py
+++ src/utils/text_to_textnodes.py
@@ -41,7 +41,7 @@ def text_to_text_nodes(text: str) -> list[TextNode]:
     old_nodes = [TextNode(text=text, type=TextType.TEXT)]
 
     old_nodes = split_nodes_code_block(old_nodes)
-    old_nodes = split_nodes_delimiter(old_nodes, "*", TextType.BOLD)
+    old_nodes = split_nodes_delimiter(old_nodes, "**", TextType.BOLD)
     old_nodes = split_nodes_delimiter(old_nodes, "_", TextType.ITALIC)
     old_nodes = split_nodes_delimiter(old_nodes, "`", TextType.CODE)
     old_nodes = split_nodes_image(old_nodes)
